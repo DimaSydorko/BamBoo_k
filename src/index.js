d8418fd8a98675c32dc7b09829b1db621d51a5ab
DimaSydorko <67498717+DimaSydorko@users.noreply.github.com>
2021-02-12 13:32:36 +0200

    add class Store in state.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import {rerenderEntireTree} from "./render" 
-import state from "./redux/state"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import store from './redux/state';
 
-rerenderEntireTree(state);
+export let rerenderEntireTree = (state) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      { <App store={store}/>}
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
 
+rerenderEntireTree(store.state);
+
+store.subscribe(rerenderEntireTree)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
